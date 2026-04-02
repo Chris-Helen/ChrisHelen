63defbbb2f2e0650e574d70863130c0d0fe81a07
Chris Helen <chrishellen.oficial@gmail.com>
2026-04-02 18:48:08 -0300

    Delete Rascunho.py

--- Rascunho.py
+++
@@ -1,4 +0,0 @@
-quantidade = float(input("Digite a quantidade de horas trabalhadas: "))
-valor = float(input("Digite o valor da hora trabalhada: "))
-salario = quantidade * valor
-print(f"O salário do funcionário é: {salario:.2f}")
