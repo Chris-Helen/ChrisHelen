10c27bf6e95424cddf4bd39b162573168b4072d9
Chris Helen <chrishellen.oficial@gmail.com>
2026-04-03 01:54:55 -0300

    Atividade py: bolinha azul

---
+++ Atividade_12.py
@@ -0,0 +1,10 @@
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+soma = a + b
+print(soma)
+if (a > b):
+    print(a , "é maior")
+elif (a < b):
+    print(b , "é maior")
+else:
+    print("Os dois números são iguais")
